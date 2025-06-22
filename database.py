a128ca8db17c4b7082ae6ba3420b939b81420ffd
raviparmar5532 <raviparmar5532@gmail.com>
2025-06-22 12:00:27 +0530

    sqlite for deployment support

--- database.py
+++ database.py
@@ -2,13 +2,13 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
-import urllib.parse
 
 load_dotenv(override=True)
 
-DB_URL = f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{urllib.parse.quote_plus(os.getenv('DB_PASSWORD'))}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+DB_URL = "sqlite:///database.db"
 
-engine = create_engine(DB_URL)
+engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base() 
 
